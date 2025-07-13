40bd0c061818fb4ac3a5e516f37b4f75ebd4fb19
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-14 04:27:18 +0800

    Create LoginActivity.kt

---
+++ app/src/main/java/com/example/ordercafemobile/LoginActivity.kt
@@ -0,0 +1,33 @@
+class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        val emailInput = findViewById<EditText>(R.id.emailEditText)
+        val passwordInput = findViewById<EditText>(R.id.passwordEditText)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val email = emailInput.text.toString()
+            val password = passwordInput.text.toString()
+            signIn(email, password)
+        }
+    }
+
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    startActivity(Intent(this, MenuActivity::class.java))
+                    finish()
+                } else {
+                    Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
